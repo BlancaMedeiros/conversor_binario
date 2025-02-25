bd8b66deef15792d37c7a7f1c7136255d9f8ffbe
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2025-02-25 19:34:25 -0300

    Criação de lógica para percorrer letra por letra de um texto.

--- conversor.py
+++ conversor.py
@@ -1,6 +1,14 @@
-print ('OPÇÕES')
-print ('1 = converter texto para binário')
+print('OPÇÕES')
+print('1 = converter texto para binário')
 
-OpcaoEscolhida = input ("Digite a opção desejada: ")
-print ("Opção escolhida: ", OpcaoEscolhida)
+OpcaoEscolhida = input("Digite a opção desejada: ")
+print("Opção escolhida: ", OpcaoEscolhida)
+
+TextoUsuario = input('degite o texto a ser convertido: ')
+print(TextoUsuario)
+
+Textoconvertido = '' 
+
+for letra in TextoUsuario:
+    print(letra)
 
