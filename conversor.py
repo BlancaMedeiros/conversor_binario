da724131ccecce299499164fd9d3d1ee86a3fa73
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2025-02-25 21:30:26 -0300

    Opção de conversão de binário para texto.

--- conversor.py
+++ conversor.py
@@ -6,13 +6,28 @@ def ConverterTextoParaBinario(texto):
         Textoconvertido = Textoconvertido + Letra_binário
     return Textoconvertido
 
+def ConverterBinarioParaTexto(TextoBinario):
+    Textoconvertido = '' 
+    quantidadeLetra = len(TextoBinario)
+    for i in range(0, int(quantidadeLetra), 7):
+        letraBinario = TextoBinario[i:i+7]
+        codigo_ascii = int(letraBinario, 2)
+        letra = chr(codigo_ascii)
+        Textoconvertido = Textoconvertido + letra
+    return Textoconvertido
+
+
 print('OPÇÕES')
 print('1 = converter texto para binário')
+print('2 = converter binário para texto')
 
 OpcaoEscolhida = input("Digite a opção desejada: ")
 print("Opção escolhida: ", OpcaoEscolhida)
 if OpcaoEscolhida == '1': 
     TextoUsuario = input('degite o texto a ser convertido: ')
     print("Resultado da conversão: ", ConverterTextoParaBinario(TextoUsuario))
+elif OpcaoEscolhida == '2':
+    TextoUsuario = input('degite o texto a ser convertido: ')
+    print("Resultado da conversão: ", ConverterBinarioParaTexto(TextoUsuario))
 else:
     print('Essa opção não existe')
